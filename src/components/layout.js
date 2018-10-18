18c0761fc84449e440aa1b4bce61ad7c33956e6d
penholder <penhold3r@gmail.com>
2018-10-18 19:46:50 -0300

    insecure mixed content fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,40 @@
-import { withRouter } from 'next/router';
-import Head from 'next/head';
+import { withRouter } from 'next/router'
+import Head from 'next/head'
 
-import Header from './header';
-import Footer from './footer';
+import Header from './header'
+import Footer from './footer'
 
-import '../styles/index.scss';
+import '../styles/index.scss'
 
 const Layout = ({ menu, children, menuClick }) => {
-
    return (
       <React.Fragment>
          <Head>
             <title>Solplay</title>
             <meta property="developer" content="penHolder Designerd" />
             <meta type="theme-color" content="#00A7E1" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1.0"
+            />
             <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <meta property="og:image" content="../../static/bookmark-image.png" />
+            <meta
+               property="og:image"
+               content="../../static/bookmark-image.png"
+            />
             <meta name="referrer" content="origin" />
-            <link rel="profile" href="http://gmpg.org/xfn/11" />
-            <link rel="shortcut icon" type="image/x-icon" href="../../static/favicon.ico" />
-            <link rel="icon" type="image/png" href="../../static/favicon.png" sizes="32x32 192x192" />
+            <link rel="profile" href="https://gmpg.org/xfn/11" />
+            <link
+               rel="shortcut icon"
+               type="image/x-icon"
+               href="../../static/favicon.ico"
+            />
+            <link
+               rel="icon"
+               type="image/png"
+               href="../../static/favicon.png"
+               sizes="32x32 192x192"
+            />
          </Head>
          <Header menu={menu} menuClick={menuClick} />
          <main>{children}</main>
@@ -29,4 +43,4 @@ const Layout = ({ menu, children, menuClick }) => {
    )
 }
 
-export default withRouter(Layout);
+export default withRouter(Layout)
